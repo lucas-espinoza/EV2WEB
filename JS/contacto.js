492853e1eab6c2e180f597bf33a823fe46c9dbf0
Lucas <lucasespinozarojas@gmail.com>
2021-12-02 20:04:10 -0300

    Creacion de script 'contacto.js', creacion formulario en contacto.html

---
+++ JS/contacto.js
@@ -0,0 +1,6 @@
+var nombre   = document.getElementById('nombre').value;
+var apellido = document.getElementById('apellido').value;
+var telefono = document.getElementById('telefono').value;
+var correo = document.getElementById('correo').value;
+
+onclick()
